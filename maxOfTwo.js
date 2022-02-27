1ecb9b6f73eb48254ca6c957693e5f259400f006
Ruth Gabriel de Sousa <ruth.sousa15@gmail.com>
2022-02-26 18:53:44 -0700

    finish exercise

---
+++ maxOfTwo.js
@@ -0,0 +1,10 @@
+let x = 1;
+let y = 2;
+
+if (y > x) {
+    console.log("y is bigger")
+} else if (x === y) {
+    console.log("The values are identical.")
+} else {
+    console.log('x is bigger')
+}
